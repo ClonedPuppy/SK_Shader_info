45a07d014aa45b7e403947f3fedeb26b5e1be4b5
Peter <peter@riel.nu>
2021-11-20 20:24:47 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SK_Shader_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
